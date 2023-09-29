66a119a88d237a175b072731f1abaff2baafebb5
rauldlnx10 <5837219+rauldlnx10@users.noreply.github.com>
2023-09-30 00:38:07 +0200

    Update install.py

--- install.py
+++ install.py
@@ -12,9 +12,9 @@ with open(req_file) as file:
                 package_name, package_version = package.split('==')
                 installed_version = pkg_resources.get_distribution(package_name).version
                 if installed_version != package_version:
-                    launch.run_pip(f"install {package}", f"PromptMaker: changing {package_name} version from {installed_version} to {package_version}")
+                    launch.run_pip(f"install {package}", f"Prompt Maker: changing {package_name} version from {installed_version} to {package_version}")
             elif not launch.is_installed(package):
-                launch.run_pip(f"install {package}", f"PromptMaker requirement: {package}")
+                launch.run_pip(f"install {package}", f"Prompt Maker requirement: {package}")
         except Exception as e:
             print(e)
             print(f'Warning: Failed to install requirements {package}.')
